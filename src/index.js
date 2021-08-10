1c167250c8fb6874af55890dc220072fa0ffd95b
nguyenminhbgbg <nguyenminhbgbg@gmail.comm>
2021-08-10 15:19:04 +0700

    commit Bootrap 4

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const port = 3000
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+
+app.use( express.static(path.join(__dirname , 'public')) )
+
+// http logger
+app.use(morgan('combined'))
+
+// template engine
+app.engine('hbs', handlebars({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname , 'resources/view'))
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
